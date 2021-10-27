de09c3abe61ec88db32b7dfb194694a64c93d88a
Lemito66 <lemito66@gmail.com>
2021-10-26 19:41:07 -0500

    Fin de Programa Subir Archivos

--- Subir Archivos/subir.php
+++ Subir Archivos/subir.php
@@ -4,7 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Confirmación</title>
+    <style>
+		body{
+			padding-left: 15px;
+			margin-left: 15px;
+		}
+		
+	</style>
 </head>
 <body>
 <?php 
@@ -19,7 +27,7 @@
             if(move_uploaded_file($guardado, 'Archivos Subidos/'.$nombre)){
                 echo "Archivo guardado con exito";
             }else{
-                echo "Archivo no se pudo guardar uuuuuu";
+                echo "Archivo no se pudo guardar";
             }
         }
     }else{
@@ -30,12 +38,21 @@
             
            <form method = "POST" action="index.php"> 
                <br>
-               <br>
-               <input type="submit" name="btn1" value="VOLVER A CARGAR ARCHIVOS">
+               
+               <input type="submit" name="btn1" value="VOLVER A CARGAR ARCHIVOS" class="btn btn-primary">
            </form>
             <?php   
         }else{
-            echo "Error al cargar archivo";
+            echo "No ha seleccionado ningun archivo";
+            ?>
+            <form action="index.php" method="post">
+                <br>
+                <input type="submit" value="Ir A PÁGINA PRINCIPAL" class="btn btn-primary">
+            </form>
+            
+            <?php 
+            
+
         }
 
         
